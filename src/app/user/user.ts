51b57f524dbd2ee2a807788d5990b527b7369edc
Nico Cerone <nikocerone96@gmail.com>
2025-11-13 20:48:07 +0100

    added DUMMY db in app.ts

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,7 +1,6 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
+import { Component, computed, Input, signal } from '@angular/core';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+/*const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);*/
 
 @Component({
   selector: 'app-user',
@@ -10,15 +9,17 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.css',
 })
 export class User {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
+  /*selectedUser = signal(DUMMY_USERS[randomIndex]);
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)*/
+  @Input({required: true}) avatar!: string
+  @Input({required: true}) name!: string;
 
-  /* get imagePath() {
-    return 'assets/users/' + this.selectedUser.avatar;
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
   }
-  */
-  onSelectedUser() {
+  
+  /*onSelectedUser() {
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
     this.selectedUser.set(DUMMY_USERS[randomIndex]) 
-  }
+  }*/
 }
